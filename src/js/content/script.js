cbadea2bd3d36e7f7f077eb3563cad6aae1b9606
shibamoto <silverbirder@gmail.com>
2019-03-06 23:40:20 +0900

    try chrome.storage

--- src/js/content/script.js
+++ src/js/content/script.js
@@ -1,3 +1,9 @@
 export default function () {
-  alert("hello! (find me on src/js/content/script.js)");
+    // chrome.storage.local.set({'key': location.href}, function () {
+    //   alert("set!")
+    // });
+    // chrome.storage.local.get("key", function (value) {
+    //     var value_data = value.key
+    //     alert(`get ${value_data}`)
+    // });
 };
